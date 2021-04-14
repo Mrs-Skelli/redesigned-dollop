0bb0c5073a6210b78373c6e028471586696a7be0
mephisto <gottaster0id1337@protonmail.com>
2021-04-14 21:28:03 +0200

    Fix up stuff and add funny features (also remove app.js because quiz.js is cooler)
    gang shit right here
    
    should work now

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 $(document).ready(function() {
-
-    $(document).on('click', '.block-link', function (e){
-
-        let flag = $(this).attr('flag');
+    $(document).on('click', '.block-link', function(e) {
         let block = $(this).closest(".question-block")
         block.css('pointer-events', 'none');
         block.css('background-color', '#F0F8FF');
         let alert = block.find(".alert");
-        alert.find('.AlertMsg').text(flag);
+        alert.find('.AlertMsg').text($(this).attr('flag'));
         alert.fadeIn('slow')
     });
-
-
 });
